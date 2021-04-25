5c86f5b630b67e807c7c29f1b7c528ee96f0234f
Yan Cao <935524537@qq.com>
2021-04-25 20:14:01 +0800

    尝试添加纹理信息--仍有bug

--- main.cpp
+++ main.cpp
@@ -1,122 +1,100 @@
 #include <vector>
 #include <cmath>
-#include <cstdlib>
 #include <limits>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
-const TGAColor green = TGAColor(0, 255, 0, 255);
-Model *model = NULL;
-const int width  = 800;
+const int width = 800;
 const int height = 800;
+const int depth = 255;
 
-//画线函数
-void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
-    bool steep = false;
-    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {//防止斜率大于1
-        std::swap(p0.x, p0.y);
-        std::swap(p1.x, p1.y);
-        steep = true;
-    }
-    if (p0.x > p1.x) {//防止斜率是负
-        std::swap(p0, p1);
-    }
+Model* model = NULL;
+int* zbuffer = NULL;
+Vec3f light_dir(0, 0, -1);
 
-    for (int x = p0.x; x <= p1.x; x++) {
-        float t = (x - p0.x) / (float)(p1.x - p0.x);
-        int y = p0.y * (1. - t) + p1.y * t + .5;
-        if (steep) {
-            image.set(y, x, color);
-        }
-        else {
-            image.set(x, y, color);
-        }
-    }
-}
+void triangle(Vec3i t0, Vec3i t1, Vec3i t2, Vec2i uv0, Vec2i uv1, Vec2i uv2, TGAImage& image, float intensity, int* zbuffer) {
+    if (t0.y == t1.y && t0.y == t2.y) return; // i dont care about degenerate triangles
+    if (t0.y > t1.y) { std::swap(t0, t1); std::swap(uv0, uv1); }
+    if (t0.y > t2.y) { std::swap(t0, t2); std::swap(uv0, uv2); }
+    if (t1.y > t2.y) { std::swap(t1, t2); std::swap(uv1, uv2); }
 
-Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
-    Vec3f s[2];
-    for (int i = 2; i--; ) {
-        s[i][0] = C[i] - A[i];
-        s[i][1] = B[i] - A[i];
-        s[i][2] = A[i] - P[i];
-    }
-    Vec3f u = cross(s[0], s[1]);
-    if (std::abs(u[2]) > 1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
-        return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
-    return Vec3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
-}
-
-void triangle(Vec3f* pts, float* zbuffer, TGAImage& image, TGAColor color) {
-    Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
-    Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
-    //根据三角形构建矩形范围
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            bboxmin[j] = std::max(0.f, std::min(bboxmin[j], pts[i][j]));
-            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
-        }
-    }
-    Vec3f P;
-    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
-        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
-            Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);//找到重心坐标的（1-u-v, u, v）
-            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;//判断是否在三角形内部，内部才着色
-            P.z = 0;
-            for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i];//计算得到P点深度值
-            if (zbuffer[int(P.x + P.y * width)] < P.z) {
-                zbuffer[int(P.x + P.y * width)] = P.z;
-                image.set(P.x, P.y, color);
+    int total_height = t2.y - t0.y;
+    for (int i = 0; i < total_height; i++) {
+        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+        float alpha = (float)i / total_height;
+        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here
+        Vec3i A = t0 + Vec3f(t2 - t0) * alpha;
+        Vec3i B = second_half ? t1 + Vec3f(t2 - t1) * beta : t0 + Vec3f(t1 - t0) * beta;
+        Vec2i uvA = uv0 + (uv2 - uv0) * alpha;
+        Vec2i uvB = second_half ? uv1 + (uv2 - uv1) * beta : uv0 + (uv1 - uv0) * beta;
+        if (A.x > B.x) { std::swap(A, B); std::swap(uvA, uvB); }
+        for (int j = A.x; j <= B.x; j++) {
+            float phi = B.x == A.x ? 1. : (float)(j - A.x) / (float)(B.x - A.x);
+            Vec3i   P = Vec3f(A) + Vec3f(B - A) * phi;
+            Vec2i uvP = uvA + (uvB - uvA) * phi;
+            int idx = P.x + P.y * width;
+            if (zbuffer[idx] < P.z) {
+                zbuffer[idx] = P.z;
+                TGAColor color = model->diffuse(uvP);
+                image.set(P.x, P.y, TGAColor(color.r * intensity, color.g * intensity, color.b * intensity));
             }
         }
     }
 }
 
-Vec3f world2screen(Vec3f v) {
-    return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
-}
-
 int main(int argc, char** argv) {
-
-    TGAImage image(width, height, TGAImage::RGB);
-    //绘制模型
-    if (2==argc) {
+    if (2 == argc) {
         model = new Model(argv[1]);
-    } else {
+    }
+    else {
         model = new Model("obj/african_head.obj");
     }
-    
-    float* zbuffer = new float[width * height];
-    for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
 
-    //漫反射
-    Vec3f light_dir(0, 0, -1);
-    for (int i=0; i<model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];
-        Vec3f pts[3];
-        for (int j=0; j<3; j++) {
-            Vec3f v = model->vert(face[j]);
-            pts[j] = world2screen(v);//转换为屏幕空间坐标
-            screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
-            world_coords[j] = v;
-        }
-        Vec3f n = cross((world_coords[2] - world_coords[0]), (world_coords[1] - world_coords[0]));
-        n.normalize();
-        float intensity = n * light_dir;
-        if (intensity > 0) {
-            triangle(pts, zbuffer, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
-        }
+    zbuffer = new int[width * height];
+    for (int i = 0; i < width * height; i++) {
+        zbuffer[i] = std::numeric_limits<int>::min();
     }
-    
-    image.flip_vertically(); // 上下翻转
-    image.write_tga_file("output.tga");
-    //delete model;
+
+    { // draw the model
+        TGAImage image(width, height, TGAImage::RGB);
+        for (int i = 0; i < model->nfaces(); i++) {
+            std::vector<int> face = model->face(i);
+            Vec3i screen_coords[3];
+            Vec3f world_coords[3];
+            for (int j = 0; j < 3; j++) {
+                Vec3f v = model->vert(face[j]);
+                screen_coords[j] = Vec3i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2., (v.z + 1.) * depth / 2.);
+                world_coords[j] = v;
+            }
+            Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+            n.normalize();
+            float intensity = n * light_dir;
+            if (intensity > 0) {
+                Vec2i uv[3];
+                for (int k = 0; k < 3; k++) {
+                    uv[k] = model->uv(i, k);
+                }
+                triangle(screen_coords[0], screen_coords[1], screen_coords[2], uv[0], uv[1], uv[2], image, intensity, zbuffer);
+            }
+        }
+
+        image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        image.write_tga_file("output.tga");
+    }
+
+    { // dump z-buffer (debugging purposes only)
+        TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                zbimage.set(i, j, TGAColor(zbuffer[i + j * width], 1));
+            }
+        }
+        zbimage.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        zbimage.write_tga_file("zbuffer.tga");
+    }
+    delete model;
+    delete[] zbuffer;
     return 0;
 }
-
