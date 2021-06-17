b04e2e963bd602ed00a5cb75f92ac857360db1a0
Yan Cao <935524537@qq.com>
2021-06-17 19:47:01 +0800

    实现Gouraud着色

--- main.cpp
+++ main.cpp
@@ -16,22 +16,20 @@ Vec3f       eye(0, -1,  3);
 Vec3f    center(0,  0,  0);
 Vec3f        up(0,  1,  0);
 
-struct FlatShader : public IShader//目前方案为Flat Shading
+struct GouraudShader : public IShader//目前方案为Flat Shading
 {
-    mat<3, 3, float>varying_tri;//我们叫这个为 varying_tri 是因为 varying是GLSL中的保留字
+    //mat<3, 3, float>varying_tri;//我们叫这个为 varying_tri 是因为 varying是GLSL中的保留字
+    Vec3f varying_intensity; // write by vertex shader, read by fragment shader
 
     virtual Vec4f vertex(int iface, int nthvert) {//顶点着色器
         Vec4f gl_vertex = embed<4>(model->vert(iface,nthvert));
-        gl_vertex = Projection * ModelView * gl_vertex; 
-        varying_tri.set_col(nthvert, proj<3>(gl_vertex / gl_vertex[3]));//记录transform后的三角形，这个在我们片段着色器决定三角形的颜色的时候使用来使用
-        gl_vertex = ViewPort * gl_vertex;
+        gl_vertex = ViewPort * Projection * ModelView * gl_vertex;
+        varying_intensity[nthvert] = CLAMP(model->normal(iface, nthvert) * light_dir); // diffuse light intensity
         return gl_vertex;
     }
 
     virtual bool fragment(Vec3f bar, TGAColor& color) {//片段着色器
-        Vec3f n = cross(varying_tri.col(1) - varying_tri.col(0), varying_tri.col(2) -
-            varying_tri.col(0)).normalize();//计算法向量
-        float intensity = CLAMP(n * light_dir);// 光强度clamp到0，1之间
+        float intensity = varying_intensity * bar; //当前像素的插值强度
         color = TGAColor(255, 255, 255) * intensity;
         return false;
     }
@@ -52,20 +50,20 @@ int main(int argc, char** argv) {
     TGAImage image(width, height, TGAImage::RGB);
     TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 
-    FlatShader shader;
+    GouraudShader shader;
     for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
         Vec4f screen_coords[3];
         for (int j = 0; j < 3; j++) {
-            screen_coords[j] = shader.vertex(i, j);
+            screen_coords[j] = shader.vertex(i, j);//处理每个三角形
         }
         triangle(screen_coords, shader, image, zbuffer);
     }
     
     image.flip_vertically(); // 上下翻转
     zbuffer.flip_vertically();
-    image.write_tga_file("output_advance.tga");
-    zbuffer.write_tga_file("zbuffer_advance.tga");
+    image.write_tga_file("output_Gouraud.tga");
+    zbuffer.write_tga_file("zbuffer_Gouraud.tga");
 
     delete model;
     return 0;
