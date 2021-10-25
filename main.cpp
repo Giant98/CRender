9a06c4b14baa0cb03703b160789d4b1ed20fa852
Yan Cao <935524537@qq.com>
2021-10-25 20:31:46 +0800

    添加SRender代码

--- main.cpp
+++ main.cpp
@@ -1,83 +1,172 @@
-#include <vector>
+#include <ctime>
 #include <iostream>
 
-#include "model.h"
-#include "our_gl.h"
+#include "./core/macro.h"
+#include "./core/tgaimage.h"
+#include "./core/model.h"
+#include "./core/camera.h"
+#include "./core/pipeline.h"
+#include "./core/sample.h"
+#include "./core/scene.h"
+#include "./platform/win32.h"
+#include "./shader/shader.h"
 
-#define CLAMP(t) ((t>1.f)?1.f:((t<0.f)?0.f:t))
+using namespace std;
 
-Model *model = NULL;
-const int width  = 600;
-const int height = 600;
+const vec3 Eye(0, 1, 5);
+const vec3 Up(0, 1, 0);
+const vec3 Target(0, 1, 0);
 
-//投影，坐标变换,光线相关变量
-Vec3f light_dir(1,  1,  1);
-Vec3f       eye(0,  1,  3);
-Vec3f    center(0,  0,  0);
-Vec3f        up(0,  1,  0);
-
-struct Shader : public IShader//目前方案为GouraudShading
+const scene_t Scenes[]
 {
-    Vec3f varying_intensity; // write by vertex shader, read by fragment shader
-    mat<2, 3, float>varying_uv;
-    mat<4, 4, float> uniform_M; //Projection*ModelView
-    mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
-
-    //iface三角面片索引，nthvert对应f数据中的索引号(0.1.2)
-    virtual Vec4f vertex(int iface, int nthvert) {//顶点着色器
-        varying_uv.set_col(nthvert, model->uv(iface, nthvert));//分别储存三角形3个点的纹理坐标
-        Vec4f gl_vertex = embed<4>(model->vert(iface,nthvert));//获取当前点的坐标信息
-        return ViewPort * Projection * ModelView * gl_vertex;
-    }
-
-    virtual bool fragment(Vec3f bar, TGAColor& color) {//片段着色器
-        Vec2f uv = varying_uv * bar;//纹理坐标插值
-        Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize(); // transform normal vector
-        Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize(); // transfrom light direction
-        Vec3f r = (n * (n * l * 2.f) - l).normalize(); // reflected light
-        float spec = pow(std::max(r.z, 0.0f), model->specular(uv)); //镜面反射 we're looking from z-axis
-        float diff = std::max(0.f, n * l);//漫反射
-        TGAColor c = model->diffuse(uv);//取纹理
-        color = c;
-        for (int i = 0; i < 3; i++) color[i] = std::min<float>(5 + c[i] * (diff + .6 * spec), 255);
-        return false;
-    }
+	{"fuhua",build_fuhua_scene},
+	{"qiyana",build_qiyana_scene},
+	{"yayi",build_yayi_scene},
+	{"xier",build_xier_scene},
+	{"helmet",build_helmet_scene},
+	{"gun",build_gun_scene},
 };
 
-int main(int argc, char** argv) {
-    //绘制模型
-    if (2==argc) {
-        model = new Model(argv[1]);
-    } else {
-        model = new Model("obj/african_head.obj");
-    }
-    
-    lookat(eye, center, up);
-    viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
-    projection(-1.f / (eye - center).norm());
-    light_dir.normalize();
+void clear_zbuffer(int width, int height, float* zbuffer);
+void clear_framebuffer(int width, int height, unsigned char* framebuffer);
+void update_matrix(Camera &camera, mat4 view_mat, mat4 perspective_mat, IShader *shader_model, IShader *shader_skybox);
 
-    TGAImage image(width, height, TGAImage::RGB);
-    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
+int main()
+{
+	// initialization
+	// --------------
+	// malloc memory for zbuffer and framebuffer
+	int width = WINDOW_WIDTH, height = WINDOW_HEIGHT;
+	float *zbuffer				= (float *)malloc(sizeof(float) * width * height);
+	unsigned char* framebuffer  = (unsigned char *)malloc(sizeof(unsigned char) * width * height * 4);//帧缓冲
+	memset(framebuffer, 0, sizeof(unsigned char) * width * height * 4);//unsigned char表示范围0-255
 
-    Shader shader;
-    shader.uniform_M = Projection * ModelView;
-    shader.uniform_MIT = (Projection * ModelView).invert_transpose();
-    for (int i = 0; i < model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);
-        Vec4f screen_coords[3];
-        for (int j = 0; j < 3; j++) {
-            screen_coords[j] = shader.vertex(i, j);//处理每个三角形，将三个顶点的intensity数据算好，返回转换后坐标
+	// create camera
+	Camera camera(Eye, Target, Up, (float)(width) / height);
+
+	// set mvp matrix
+	mat4 model_mat			= mat4::identity();
+	mat4 view_mat			= mat4_lookat(camera.eye, camera.target, camera.up);
+	mat4 perspective_mat	= mat4_perspective(60, (float)(width)/height, -0.1, -10000);
+
+	// initialize models and shaders by builidng a scene
+	srand((unsigned int)time(NULL));
+	int scene_index = rand() % 6;
+	int model_num = 0;
+	Model	*model[MAX_MODEL_NUM];
+	IShader *shader_model;
+	IShader *shader_skybox;
+	Scenes[scene_index].build_scene(model, model_num, &shader_model, &shader_skybox, perspective_mat, &camera);
+
+	// initialize window
+	window_init(width, height, "SRender");
+
+	// render loop
+	// -----------
+	int num_frames = 0;
+	float print_time = platform_get_time();
+	while (!window->is_close)
+	{
+		float curr_time = platform_get_time();
+
+		// clear buffer
+		clear_framebuffer(width, height, framebuffer);
+		clear_zbuffer(width, height, zbuffer);
+
+		// handle events and update view, perspective matrix
+		handle_events(camera);
+		update_matrix(camera, view_mat, perspective_mat, shader_model, shader_skybox);
+
+		// draw models
+		for (int m = 0; m < model_num; m++)
+		{
+			// assign model data to shader
+			shader_model->payload.model = model[m];
+			if(shader_skybox != NULL) shader_skybox->payload.model = model[m];
+			
+			// select current shader according model type
+			IShader *shader;
+			if (model[m]->is_skybox)
+				shader = shader_skybox;
+			else
+				shader = shader_model;
+
+			for (int i = 0; i < model[m]->nfaces(); i++)
+			{
+				draw_triangles(framebuffer, zbuffer, *shader, i);
+			}
+		}
+
+		// calculate and display FPS
+		num_frames += 1;
+        if (curr_time - print_time >= 1) {
+            int sum_millis = (int)((curr_time - print_time) * 1000);
+            int avg_millis = sum_millis / num_frames;
+            printf("fps: %3d, avg: %3d ms\n", num_frames, avg_millis);
+            num_frames = 0;
+            print_time = curr_time;
         }
-        triangle(screen_coords, shader, image, zbuffer);
-    }
-    
-    image.flip_vertically(); // 上下翻转
-    zbuffer.flip_vertically();
-    image.write_tga_file("./result/output_Phonglight.tga");
-    zbuffer.write_tga_file("./result/zbuffer_Phonglight.tga");
 
-    delete model;
-    return 0;
+		// reset mouse information
+		window->mouse_info.wheel_delta = 0;
+		window->mouse_info.orbit_delta = vec2(0,0);
+		window->mouse_info.fv_delta = vec2(0, 0);
+
+		// send framebuffer to window 
+		window_draw(framebuffer);
+		msg_dispatch();
+	}
+
+
+	// free memory
+	for (int i = 0; i < model_num; i++)
+		if (model[i] != NULL)  delete model[i];
+	if (shader_model != NULL)  delete shader_model;
+	if (shader_skybox != NULL) delete shader_skybox;
+	free(zbuffer);
+	free(framebuffer);
+	window_destroy();
+
+	system("pause");
+	return 0;
 }
 
+
+void clear_zbuffer(int width, int height, float* zbuffer)
+{
+	for (int i = 0; i < width*height; i++)
+		zbuffer[i] = 100000;
+}
+
+void clear_framebuffer(int width, int height, unsigned char* framebuffer)
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			int index = (i * width + j) * 4;
+
+			framebuffer[index + 2] = 80;
+			framebuffer[index + 1] = 56;
+			framebuffer[index] = 56;
+		}
+	}
+}
+
+void update_matrix(Camera &camera, mat4 view_mat, mat4 perspective_mat, IShader *shader_model, IShader *shader_skybox)
+{
+	view_mat = mat4_lookat(camera.eye, camera.target, camera.up);
+	mat4 mvp = perspective_mat * view_mat;
+	shader_model->payload.camera_view_matrix = view_mat;
+	shader_model->payload.mvp_matrix = mvp;
+
+	if (shader_skybox != NULL)
+	{
+		mat4 view_skybox = view_mat;
+		view_skybox[0][3] = 0;
+		view_skybox[1][3] = 0;
+		view_skybox[2][3] = 0;
+		shader_skybox->payload.camera_view_matrix = view_skybox;
+		shader_skybox->payload.mvp_matrix = perspective_mat * view_skybox;
+	}
+}
